c88ff45016c152df916ddd1edaa1c1267d9c7e59
Herbert Amaro Santana <herbert.webdesigner@gmail.com>
2022-10-31 16:54:14 -0300

    Instalar Github

---
+++ layout/header.js
@@ -0,0 +1,47 @@
+import Image from "next/image"
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Link from 'next/Link';
+
+
+export default function Header() {  
+    return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand>
+            <Link href="/">
+                <Image src="/logo.gif" width={300} height={60} />
+            </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll" className="ml-auto">
+          <Nav
+            className="ml-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px', justifyContent: 'end' }}
+            navbarScroll
+          >
+            <Nav.Item>
+            <Link href="/">Home</Link>
+            </Nav.Item>
+            <Nav.Item>
+            <Link href="/curriculum" passHref>Curriculum</Link>
+            </Nav.Item>
+            <Nav.Item>
+            <Link href="/#work">Work</Link>
+            </Nav.Item>
+            <Nav.Item>
+            <Link href="/contact">Contact</Link>
+            </Nav.Item>
+            <Nav.Item className="phone">
+            <strong>+55 11 985988444</strong>
+            </Nav.Item>
+            <Nav.Link href="#action2">EN</Nav.Link>
+            <Nav.Link href="#action2">PT</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    )
+
+}
---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/index.css';
+import '../styles/cv.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
